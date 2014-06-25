3925aff5cba3600c6fd5cedae418b93df3ee2921
K - Peggy <peggy110336@gamil.com>
2014-06-25 19:33:12 +0800

    UVA458 -The Decoder
    處理負數狀況。
    System.out.print();  改成  System.out.write();
    (解決逾時問題)

--- 458/main.java
+++ 458/main.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Scanner enter = new Scanner(System.in);
 		String s;
+		char c;
 		
 		while(enter.hasNext()) {
 			s = enter.nextLine();
 			for(int i=0;i<s.length();i++) {
-				System.out.print((char)(s.charAt(i)-7));
+				c = (char)(s.charAt(i)-7);
+				if((s.charAt(i)-7)>0)
+					System.out.write( c );
 			}
 			System.out.println();
 		}
